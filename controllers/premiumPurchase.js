e8aa979c181d2e9ff8c76c31f4e72fdc0cdd5bf4
jyotirmyee <haniyajo45@gmail.com>
2022-12-23 07:49:24 +0530

    optimising premium features using sql join

--- controllers/premiumPurchase.js
+++ controllers/premiumPurchase.js
@@ -2,34 +2,30 @@
 
 const Expense=require('../models/expenses')
 const User=require('../models/users')
+const sequelize=require('../utill/database')
 
 
 exports.getUserLeaderboard= async (req,res)=>{
     try{
-        const users= await User.findAll();
-        const expenses= await  Expense.findAll();
-        const totalUserExpenses={}
-       
-        console.log(expenses)
-      
-        expenses.forEach((expense)=>{
-          if(totalUserExpenses[expense.userId]){
-            totalUserExpenses[expense.userId]+=expense.Expenseamount;
-          }
-          else{
-            totalUserExpenses[expense.userId]=expense.Expenseamount
-          }
-        })
-       var userLeaderboardDetails=[]
-       users.forEach((user)=>{
-        userLeaderboardDetails.push({name:user.name,total_expenses:totalUserExpenses[user.id] || 0})
-       })
-       console.log( userLeaderboardDetails)
-       //sorting the array to arrange the user
-       userLeaderboardDetails.sort((a,b)=>b.total_expenses-a.total_expenses)
-       res.status(200).json( userLeaderboardDetails)
+        const leaderboardOfUsers= await User.findAll({
+          attributes:['id','name',[sequelize.fn('sum',sequelize.col('expenses.Expenseamount')),'total_expenses']],
+          include:[{
+            model:Expense,
+            attributes:[]
 
-    }catch(err){
+          }//sql join table user and expense
+           
+          ],
+          group:['user.id'],//grouping user as user.id to compare there expenses
+          order:[[sequelize.col('total_expenses'),'DESC']]  //sorting 
+
+
+          
+        })
+        res.status(200).json(leaderboardOfUsers)
+       
+       
+        }catch(err){
       console.log(err)
     }
 
